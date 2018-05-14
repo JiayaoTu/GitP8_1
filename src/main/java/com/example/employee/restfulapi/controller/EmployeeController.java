8b5a76c2c421467a8709081c0e8a46088f80f1c5
Kingtanna <1435462374@qq.com>
2018-05-14 20:30:49 +0800

    employees

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,5 +1,24 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping(value="/employees")
+@RestController
 public class EmployeeController {
     //在此处完成Employee API
+    @Autowired
+    EmployeeRepository employeeRepository;
+    //获取employee列表
+    @GetMapping
+    List<Employee> findEmployees()
+    {
+        return employeeRepository.findAll();
+    }
 }
