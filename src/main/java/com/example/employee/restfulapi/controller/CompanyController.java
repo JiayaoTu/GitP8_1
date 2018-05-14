0bf7d33a708fbdbbaa0eefc073a025cffcab76f4
Kingtanna <1435462374@qq.com>
2018-05-14 19:49:51 +0800

    companies/1

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -4,19 +4,27 @@ import com.example.employee.restfulapi.entity.Company;
 import com.example.employee.restfulapi.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/companies")
 public class CompanyController {
     //在此处完成Company API
     @Autowired
     private CompanyRepository companyRepository;
 
-    @GetMapping(value="/companies")
+    @GetMapping
     public List<Company> employeeList() {
         return companyRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Company getCompany(@PathVariable(value = "id") Long id) {
+        return companyRepository.findOne(id);
     }
 
 }
