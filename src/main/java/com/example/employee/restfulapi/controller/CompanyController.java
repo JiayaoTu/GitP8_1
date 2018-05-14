3868ac6e1f9af5e7003e4fcbe1a6a471d3489b03
Kingtanna <1435462374@qq.com>
2018-05-14 19:54:59 +0800

    companies/1/employees

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -14,17 +14,26 @@ import java.util.List;
 @RequestMapping(value = "/companies")
 public class CompanyController {
     //在此处完成Company API
+
     @Autowired
     private CompanyRepository companyRepository;
 
+    //获取company列表
     @GetMapping
     public List<Company> employeeList() {
         return companyRepository.findAll();
     }
 
+    //获取某个具体company
     @GetMapping(value = "/{id}")
     public Company getCompany(@PathVariable(value = "id") Long id) {
         return companyRepository.findOne(id);
+    }
+
+    //获取某个具体company下所有employee列表
+    @GetMapping(value = "/{id}/employees")
+    public List getEmployee(@PathVariable(value = "id") Long id) {
+        return companyRepository.findOne(id).getEmployees();
     }
 
 }
