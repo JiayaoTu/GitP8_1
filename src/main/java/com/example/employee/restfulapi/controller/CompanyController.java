e2a98524c7ccdb408c4f5f79dfa85ca68a314768
Kingtanna <1435462374@qq.com>
2018-05-14 20:03:20 +0800

    addCompany

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,5 +42,12 @@ public class CompanyController {
         Pageable pageable = new PageRequest(pageNo, pageSize);
         return companyRepository.findAll(pageable);
     }
+
+    //增加一个company
+    @PostMapping
+    public Company addCompany(Company company) {
+        return companyRepository.save(company);
+    }
+
 
 }
