a00b64edbb89b8dcbde70cf5e6b7c420d9755ee3
Kingtanna <1435462374@qq.com>
2018-05-14 20:39:38 +0800

    employees/1employees/1

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
 import com.example.employee.restfulapi.entity.Employee;
 import com.example.employee.restfulapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +32,12 @@ public class EmployeeController {
     Employee getEmployee(@PathVariable(value = "id") Long id)
     {
         return employeeRepository.findOne(id);
+    }
+
+    //分页查询，page等于1，pageSize等于5
+    @GetMapping(value = "/page/{pageNo}/pageSize/{pageSize}")
+    public Page<Employee> getEmployeePage(@PathVariable(value = "pageNo") int pageNo, @PathVariable(value = "pageSize") int pageSize) {
+        Pageable pageable = new PageRequest(pageNo, pageSize);
+        return employeeRepository.findAll(pageable);
     }
 }
